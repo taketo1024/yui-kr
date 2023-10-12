5ef0d77ba6396558323ba7d78c116ed53ca5ff43
Taketo Sano <taketo1024@gmail.com>
2023-10-12 11:50:05 +0900

    add tests.

--- src/kr/hor_homol.rs
+++ src/kr/hor_homol.rs
@@ -6,7 +6,7 @@ use yui_core::{EucRing, EucRingOps};
 use yui_homology::{FreeChainComplex, ChainComplex, RModStr};
 use yui_homology::utils::HomologyCalc;
 use yui_lin_comb::LinComb;
-use yui_matrix::sparse::SpMat;
+use yui_matrix::sparse::{SpMat, MatType};
 use yui_utils::bitseq::BitSeq;
 use crate::kr::hor_cube::KRHorCube;
 
@@ -35,15 +35,15 @@ where R: EucRing, for<'x> &'x R: EucRingOps<R> {
     }
 
     fn compute_summand(&self, i: usize) -> KRHorHomolSummand<R> { 
-        let j = i as isize;
-        let d1 = self.complex.d_matrix(j);
-        let d2 = self.complex.d_matrix(j+1);
+        let i = i as isize;
+        let d0 = self.complex.d_matrix(i - 1);
+        let d1 = self.complex.d_matrix(i);
 
         // p: (r, n), q: (n, r)
-        let (h, p, q) = HomologyCalc::calculate_with_trans(d1, d2);
+        let (h, p, q) = HomologyCalc::calculate_with_trans(d0, d1);
         let r = h.rank();
 
-        let gens = self.complex[j].generators();
+        let gens = self.complex[i].generators();
         let h_gens = (0..r).map(|k| { 
             let v = q.col_vec(k); 
             let terms = v.iter().map(|(i, a)| {
@@ -68,5 +68,51 @@ where R: EucRing, for<'x> &'x R: EucRingOps<R> {
 
     pub fn gens(&self, i: usize) -> &Vec<LinComb<VertGen, R>> { 
         &self.summand(i).0
+    }
+}
+
+#[cfg(test)]
+mod tests { 
+    use num_traits::Zero;
+    use yui_link::Link;
+    use yui_ratio::Ratio;
+    use crate::kr::base::EdgeRing;
+    use super::*;
+
+    type R = Ratio<i64>;
+    type P = EdgeRing<R>;
+
+    fn make_hml(l: &Link, v: BitSeq, q: isize) -> KRHorHomol<R> {
+        let data = KRCubeData::<R>::new(&l);
+        let rc = Rc::new(data);
+        KRHorHomol::new(rc, v, q)
+    }
+
+    #[test]
+    fn rank() { 
+        let l = Link::trefoil();
+        let v = BitSeq::from_iter([1,0,0]);
+        let q = 0;
+        let hml = make_hml(&l, v, q);
+
+        assert_eq!(hml.rank(0), 0);
+        assert_eq!(hml.rank(1), 0);
+        assert_eq!(hml.rank(2), 1);
+        assert_eq!(hml.rank(3), 1);
+    }
+
+    #[test]
+    fn gens() { 
+        let l = Link::trefoil();
+        let v = BitSeq::from_iter([1,0,0]);
+        let q = 0;
+        let hml = make_hml(&l, v, q);
+
+        let zs = hml.gens(2);
+        assert_eq!(zs.len(), 1);
+
+        let z = &zs[0];
+        let dz = hml.complex.differetiate(z); 
+        assert!(dz.is_zero());
     }
 }
