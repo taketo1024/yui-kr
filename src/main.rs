f2c46ab16b7719eb5828f2d5d62269539a67be45
Taketo Sano <taketo1024@gmail.com>
2023-11-03 20:43:08 +0900

    modify main.

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,26 @@ use yui_link::Link;
 use yui_ratio::Ratio;
 
 fn main() {
+    measure(|| { 
+        run();
+    })
+}
+
+fn run() { 
     type R = Ratio<i64>;
-    
+
     let l = Link::trefoil();
     let h = KRHomology::<R>::new(&l);
-
     h.print_table();
+}
+
+fn measure<F, Res>(proc: F) -> Res
+where F: FnOnce() -> Res { 
+    let start = std::time::Instant::now();
+    let res = proc();
+    let time = start.elapsed();
+
+    println!("time: {:?}", time);
+
+    res
 }
