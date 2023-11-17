38c14e75e26a7a593e2d19ce61f18da7fdf26d1b
Taketo Sano <taketo1024@gmail.com>
2023-11-17 14:48:06 +0900

    App::dispatch

--- src/bin/app/app.rs
+++ src/bin/app/app.rs
@@ -1,34 +1,65 @@
 use log::{info, error};
 use clap::Parser;
+use derive_more::Display;
+use yui::Ratio;
+use yui_homology::DisplayTable;
+use yui_kr::kr::KRHomology;
+use yui_link::Braid;
 use super::utils::*;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 #[command(propagate_version = true)]
 pub struct CliArgs {
+    target: String,
+
     #[arg(long)]
-    pub debug: bool
+    debug: bool
 }
 
-pub struct App {}
+#[derive(Debug, Display)]
+pub struct AppErr(pub(crate) String);
+impl std::error::Error for AppErr {}
+
+macro_rules! err {
+    ($($arg:tt)*) => {{
+        let msg = format!($($arg)*);
+        let e = AppErr(msg);
+        std::result::Result::Err(e.into())
+    }}
+}
+
+pub(crate) use err;
+
+pub struct App {
+    args: CliArgs
+}
 
 impl App { 
     pub fn new() -> Self { 
-        App {}
+        let args = CliArgs::parse();
+        if args.debug { 
+            Self::init_logger();
+        }
+        App { args }
+    }
+
+    fn init_logger() {
+        use simplelog::*;
+        TermLogger::init(
+            LevelFilter::Info,
+            Config::default(),
+            TerminalMode::Mixed,
+            ColorChoice::Auto
+        ).unwrap()
     }
 
     pub fn run(&self) -> Result<String, i32> { 
-        let args = CliArgs::parse();
+        info!("args: {:?}", self.args);
 
-        if args.debug { 
-            self.init_logger();
-        }
-
-        info!("args: {:?}", args);
-
-        let (res, time) = measure(||
-            self.dispatch(&args)
-        );
+        let (res, time) = measure(|| guard_panic(||
+            self.dispatch()
+        ));
 
         let res = res.map_err(|e| { 
             error!("{}", e);
@@ -41,40 +72,16 @@ impl App {
         res
     }
 
-    fn init_logger(&self) {
-        use simplelog::*;
-        TermLogger::init(
-            LevelFilter::Info,
-            Config::default(),
-            TerminalMode::Mixed,
-            ColorChoice::Auto
-        ).unwrap()
-    }
+    fn dispatch(&self) -> Result<String, Box<dyn std::error::Error>> { 
+        type R = Ratio<i128>;
 
-    fn dispatch(&self, args: &CliArgs) -> Result<String, Box<dyn std::error::Error>> { 
-        guard_panic(||
-            Ok("todo".into())
-        )
+        let target = &self.args.target;
+        let braid = Braid::load(target)?;
+        let link = braid.closure();
+
+        let kr = KRHomology::<R>::new(&link);
+        let res = kr.display_table();
+
+        Ok(res)
     }
 }
-
-#[derive(Debug, derive_more::Display)]
-pub struct AppErr(pub(crate) String);
-
-impl AppErr { 
-    pub fn msg(&self) -> &str { 
-        &self.0
-    }
-}
-
-impl std::error::Error for AppErr {}
-
-macro_rules! err {
-    ($($arg:tt)*) => {{
-        let msg = format!($($arg)*);
-        let e = AppErr(msg);
-        std::result::Result::Err(e.into())
-    }}
-}
-
-pub(crate) use err;
