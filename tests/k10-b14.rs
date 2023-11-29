306078f38e8bda3a695e3a286ad092d0e8116f85
Taketo Sano <taketo1024@gmail.com>
2023-11-29 18:21:32 +0900

    extract k10-b14.

---
+++ tests/k10-b14.rs
@@ -0,0 +1,11 @@
+#![cfg(feature = "batch_test")]
+
+mod common;
+use common::test;
+
+test!(k10_67, "10_67");
+test!(k10_68, "10_68");
+test!(k10_74, "10_74");
+test!(k10_97, "10_97");
+test!(k10_101, "10_101");
+test!(k10_120, "10_120");
