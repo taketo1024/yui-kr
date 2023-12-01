ffa0b373c0313c30d2563716cf55468a48fe11c9
Taketo Sano <taketo.sano@ithems_fe02.intra.riken.jp>
2023-12-01 10:35:45 +0900

    add batch scripts and logs.

---
+++ batch/k11n.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+#PBS -q C12-LONG
+#PBS -l select=1:ncpus=48:mpiprocs=1
+#PBS -l walltime=72:00:00
+#PBS -M taketo.sano@riken.jp
+#PBS -m abe
+#PBS -j oe
+#PBS -o /home/taketo.sano/logs/
+#PBS -N yui-kr_k11n
+
+echo "running yui-kr..."
+
+source /home/taketo.sano/.cargo/env
+cd /home/taketo.sano/yui-kr/
+
+cargo update
+cargo test -r --features batch_test --test k11n -- --test-threads=1 --show-output
