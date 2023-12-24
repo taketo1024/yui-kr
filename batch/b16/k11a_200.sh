6e62e33156a4aca8d9f73baadd9392d8cbe1c687
Taketo Sano <taketo.sano@ithems_fe02.intra.riken.jp>
2023-12-24 19:03:28 +0900

    update batch.

---
+++ batch/b16/k11a_200.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+#PBS -q F
+#PBS -l select=1:ncpus=64:mpiprocs=1
+#PBS -l walltime=12:00:00
+#PBS -M taketo.sano@riken.jp
+#PBS -m abe
+#PBS -j oe
+#PBS -o /home/taketo.sano/logs/
+#PBS -N yui-kr_k11a_200
+
+echo "running yui-kr..."
+
+source /home/taketo.sano/.cargo/env
+cd /home/taketo.sano/yui-kr/
+
+cargo update
+cargo run -r -- 11a_200 -i bigint -s -p --debug
