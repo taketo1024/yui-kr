fe20b192444fb6dd1be6d87242ca09a888ca06c6
Taketo Sano <taketo1024@gmail.com>
2024-01-04 16:52:39 +0900

    extract KRTotComplexReducer.

--- src/lib/internal/tot_cpx.rs
+++ src/lib/internal/tot_cpx.rs
@@ -202,68 +202,9 @@ where R: EucRing, for<'x> &'x R: EucRingOps<R> {
     }
 
     pub fn reduced(&self) -> ChainComplex2<R> {
-        info!("reduce C_tot (q: {})..", self.q);
-
-        let mut reducer = ChainReducer::new(self.support(), self.d_deg(), false);
-
-        for idx in self.support() {
-            let to_idx = idx + self.d_deg();
-            
-            if self.rank(idx).is_zero() { 
-                let m = self.rank(to_idx);
-                reducer.set_matrix(idx, SpMat::zero((m, 0)));
-                continue;
-            }
-
-            info!("d {idx} -> {to_idx}");
-
-            // MEMO: The 'next matrix' will serve as trans-back for the current one. 
-            // This is to reduce the input dim without `with_trans = true`.
-            
-            let d = if let Some(q) = reducer.matrix(idx) {
-                self.d_matrix_for(idx, &q)
-            } else { 
-                self.d_matrix(idx)
-            };
-
-            info!("  size:    {:?}", d.shape());
-
-            if self.is_supported(to_idx) { 
-                let m = d.nrows();
-                reducer.set_matrix(to_idx, SpMat::id(m));
-            }
-
-            reducer.set_matrix(idx, d);
-            reducer.reduce_at(idx, false);
-
-            info!("  reduced: {:?}", reducer.matrix(idx).unwrap().shape());
-        }
-
-        if !reducer.is_done() { 
-            info!("second run..");
-
-            for idx in self.support() {
-                let to_idx = idx + self.d_deg();
-                let d = reducer.matrix(idx).unwrap();
-                
-                if d.is_zero() { 
-                    continue;
-                }
-    
-                info!("d {idx} -> {to_idx}");
-                info!("  size:    {:?}", d.shape());
-    
-                reducer.reduce_at(idx, true);
-    
-                info!("  reduced: {:?}", reducer.matrix(idx).unwrap().shape());
-            }    
-        }
-
-        let red = reducer.into_complex();
-
-        info!("reduced C (q: {})\n{}", self.q, red.display_table("h", "v"));
-
-        red
+        let mut reducer = KRTotComplexReducer::new(self);
+        reducer.reduce();
+        reducer.into_complex()
     }
 }
 
@@ -330,6 +271,100 @@ where R: EucRing, for<'x> &'x R: EucRingOps<R> {
     }
 }
 
+pub struct KRTotComplexReducer<'a, R>
+where R: EucRing, for<'x> &'x R: EucRingOps<R> { 
+    complex: &'a KRTotComplex<R>,
+    reducer: ChainReducer<isize2, R>
+}
+
+impl<'a, R> KRTotComplexReducer<'a, R> 
+where R: EucRing, for<'x> &'x R: EucRingOps<R> {
+    pub(crate) fn new(complex: &'a KRTotComplex<R>) -> Self { 
+        let reducer = ChainReducer::new(
+            complex.support(), 
+            complex.d_deg(), 
+            false
+        );
+        Self { complex, reducer }
+    }
+
+    pub(crate) fn reduce(&mut self) { 
+        info!("reduce C_tot (q: {})..", self.complex.q);
+
+        self.reduce_initial();
+
+        if !self.reducer.is_done() { 
+            info!("second run..");
+
+            self.reduce_second();
+        }
+    }
+
+    fn reduce_initial(&mut self) { 
+        for idx in self.complex.support() {
+            let to_idx = idx + self.complex.d_deg();
+            
+            if self.complex.rank(idx).is_zero() { 
+                let m = self.complex.rank(to_idx);
+                self.reducer.set_matrix(idx, SpMat::zero((m, 0)));
+                continue;
+            }
+
+            info!("d {idx} -> {to_idx}");
+
+            // MEMO: The 'next matrix' will serve as trans-back for the current one. 
+            // This is to reduce the input dim without `with_trans = true`.
+            
+            let d = if let Some(q) = self.reducer.matrix(idx) {
+                self.complex.d_matrix_for(idx, &q)
+            } else { 
+                self.complex.d_matrix(idx)
+            };
+
+            if self.complex.is_supported(to_idx) { 
+                let m = d.nrows();
+                self.reducer.set_matrix(to_idx, SpMat::id(m));
+            }
+
+            self.reducer.set_matrix(idx, d);
+
+            self.reduce_at(idx, false);
+        }
+    }
+
+    fn reduce_second(&mut self) { 
+        for idx in self.complex.support() {
+            let to_idx = idx + self.complex.d_deg();
+            let d = self.reducer.matrix(idx).unwrap();
+            
+            if d.is_zero() { 
+                continue;
+            }
+
+            info!("d {idx} -> {to_idx}");
+
+            self.reduce_at(idx, false);
+        }    
+    }
+
+    fn reduce_at(&mut self, idx: isize2, deep: bool) { 
+        let d = self.reducer.matrix(idx).unwrap();
+
+        info!("  size:    {:?}", d.shape());
+
+        self.reducer.reduce_at(idx, deep);
+
+        info!("  reduced: {:?}", self.reducer.matrix(idx).unwrap().shape());
+    }
+
+    pub(crate) fn into_complex(self) -> ChainComplex2<R> { 
+        let red = self.reducer.into_complex();
+
+        info!("reduced C (q: {})\n{}", self.complex.q, red.display_table("h", "v"));
+        
+        red
+    }
+}
 
 #[cfg(test)]
 mod tests { 
