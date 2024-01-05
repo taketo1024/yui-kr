65cf5c5084bbab3c9d558ce30400931d30d0b696
Taketo Sano <taketo1024@gmail.com>
2024-01-05 14:54:11 +0900

    remove hard_* tests.

--- tests/hard_k11-b17.rs
+++
@@ -1,7 +0,0 @@
-#![cfg(feature = "batch_test")]
-
-mod common;
-use common::test;
-
-test!(k11a_280, "11a_280");
-test!(k11a_362, "11a_362");
--- tests/hard_k9-b14.rs
+++
@@ -1,6 +0,0 @@
-#![cfg(feature = "batch_test")]
-
-mod common;
-use common::test;
-
-test!(k9_35, "9_35");
